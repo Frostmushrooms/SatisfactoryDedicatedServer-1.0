347cfe9be0d6a745d61f2fbbb44992d5f8ded32c
Frostmushrooms <82301326+Frostmushrooms@users.noreply.github.com>
2024-09-15 20:52:30 +0800

    Update Game download update file.bat

--- SatisfactoryDedicatedServer/Game download update file.bat
+++ SatisfactoryDedicatedServer/Game download update file.bat
@@ -1,1 +1,2 @@
-taskkill /IM FactoryServer-Win64-Shipping-Cmd.exe /F & start C:\SatisfactoryDedicatedServer\steamcmd\steamcmd.exe +force_install_dir C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer +login anonymous +app_update 1690800 -beta public validate +quit
+@echo off
+taskkill /f /im cmd.exe /F & taskkill /IM FactoryServer-Win64-Shipping-Cmd.exe /F & start C:\SatisfactoryDedicatedServer\steamcmd\steamcmd.exe +force_install_dir C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer +login anonymous +app_update 1690800 -beta public validate +quit
