8741be6898d694b1e4bf8f322c13856e93e56d86
Frostmushrooms <82301326+Frostmushrooms@users.noreply.github.com>
2024-09-17 21:27:58 +0800

    Update start.bat

--- SatisfactoryDedicatedServer/start.bat
+++ SatisfactoryDedicatedServer/start.bat
@@ -11,10 +11,10 @@ set PORT=7777
 set "found="
 for /F "tokens=2 delims=:" %%a in ('netstat -a -n -o -p UDP ^| findstr :%PORT%') do set "found=%%a"
 if not defined found (
-    echo %date%%time% 幸福工厂服务器已离线. 正在重新启动服务器
+    echo %date%%time% 幸福工厂服务器已离线. 正在重新启动服务器 请耐心等待
 taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe & start C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer\FactoryServer.exe -log -unattended -port=7777
 )
  
-echo %date%%time% 幸福工厂服务器正在工作. 冰霜蘑菇QQ:1056484009
+echo %date%%time% 幸福工厂服务器正在工作. 冰霜蘑菇 QQ:1056484009 QQ群:264127585 
 timeout /T 30 >nul
 goto check_port
