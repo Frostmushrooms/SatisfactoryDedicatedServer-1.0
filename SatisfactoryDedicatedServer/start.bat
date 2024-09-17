5d51c50d1060440ea4d6b4264abff8ccb36c07b9
Frostmushrooms <82301326+Frostmushrooms@users.noreply.github.com>
2024-09-18 00:52:08 +0800

    Add files via upload

--- SatisfactoryDedicatedServer/start.bat
+++ SatisfactoryDedicatedServer/start.bat
@@ -1,23 +1,19 @@
 @echo off
-echo You are starting the SatisfactoryDedicatedServer in daemon mode 
-echo 您正在启动幸福工厂服务器 守护模式
-
-taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe
-
+echo start server
+taskkill /IM FactoryServer-Win64-Shipping-Cmd.exe /F
 setlocal
  
 set PORT=7777
+set APP_PATH=C:\SatisfactoryDedicatedServer\start.bat
  
 :check_port
 set "found="
 for /F "tokens=2 delims=:" %%a in ('netstat -a -n -o -p UDP ^| findstr :%PORT%') do set "found=%%a"
 if not defined found (
-    echo %date%%time% 幸福工厂服务器已离线. 正在重新启动服务器 请耐心等待
-    echo %date%%time% The SatisfactoryDedicatedServer is offline. The server is restarting. Please wait patiently.
-taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe & start C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer\FactoryServer.exe -log -unattended -port=7777
+    echo Port %PORT% is not in use. Opening %APP_PATH%
+    start C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer\FactoryServer.exe -log -unattended
 )
  
-echo %date%%time% SatisfactoryDedicatedServer is working 
-echo %date%%time%  幸福工厂服务器正在工作. 冰霜蘑菇 QQ:1056484009 QQ群:264127585
+echo Port %PORT% is in use.
 timeout /T 30 >nul
 goto check_port
