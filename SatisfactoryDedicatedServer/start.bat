0b0f37272a17db8730979ac159465d6dd6776617
Frostmushrooms <82301326+Frostmushrooms@users.noreply.github.com>
2024-09-18 00:58:49 +0800

    Update start.bat

--- SatisfactoryDedicatedServer/start.bat
+++ SatisfactoryDedicatedServer/start.bat
@@ -1,20 +1,20 @@
 @echo off
-echo 您正在启动幸福工厂服务器 守护模式
+echo You are starting the SatisfactoryDedicatedServer in daemon mode
 
 taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe
 
 setlocal
  
-set PORT=9999
+set PORT=7777
  
 :check_port
 set "found="
 for /F "tokens=2 delims=:" %%a in ('netstat -a -n -o -p UDP ^| findstr :%PORT%') do set "found=%%a"
 if not defined found (
-    echo %date%%time% 幸福工厂服务器已离线. 正在重新启动服务器 请耐心等待
-taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe & start C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer\FactoryServer.exe -log -unattended -port=9999
+    echo %date%%time% SatisfactoryDedicatedServer is offline. Restarting the server, please be patient
+taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe & start C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer\FactoryServer.exe -log -unattended -port=7777
 )
  
-echo %date%%time% 幸福工厂服务器正在工作. 冰霜蘑菇 QQ:1056484009 QQ群:264127585 
+echo %date%%time% SatisfactoryDe鈥嬧�媎icatedServer is working.  QQ:1056484009 QQgroup:264127585 
 timeout /T 30 >nul
 goto check_port
