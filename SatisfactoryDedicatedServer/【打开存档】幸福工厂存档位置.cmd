896fa0f3c0ba23e1f267c210786735e08bd31aa0
Frostmushrooms <82301326+Frostmushrooms@users.noreply.github.com>
2024-09-22 11:52:53 +0800

    Add files via upload
    Added code to open SatisfactoryDedicatedServer archive location

---
+++ SatisfactoryDedicatedServer/【打开存档】幸福工厂存档位置.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+explorer.exe %USERPROFILE%\AppData\Local\"FactoryGame"\"Saved"\SaveGames
+
+
+echo    moguwu         
+echo    幸福工厂/异星工厂建造群   
+echo    Q群：264127585        
+echo    群游戏论坛地址：https://www.sxtvip.top/forums/
+
+PAUSE>>nul
