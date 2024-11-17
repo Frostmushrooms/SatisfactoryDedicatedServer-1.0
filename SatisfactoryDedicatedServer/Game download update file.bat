024e4cb414acad8ae166f0d8b371674bf6471f1b
Frostmushrooms <82301326+Frostmushrooms@users.noreply.github.com>
2024-11-17 11:26:44 +0800

    Update Game download update file.bat

--- SatisfactoryDedicatedServer/Game download update file.bat
+++ SatisfactoryDedicatedServer/Game download update file.bat
@@ -1,8 +1,8 @@
 if not exist "%~dp0steamcmd" (
     mkdir "%~dp0steamcmd"
-		CD "%~dp0steamcmd"
-	powershell curl -o "steamcmd.zip" "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
-    tar -xf steamcmd.zip
+	 CD "%~dp0steamcmd"
+     curl -o "steamcmd.zip" "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+     tar -xf steamcmd.zip
 )
 taskkill /IM FactoryServer-Win64-Shipping-Cmd.exe /F
 "%~dp0steamcmd/steamcmd.exe" +force_install_dir "%~dp0SatisfactoryServer" +login anonymous +app_update 1690800 -beta public validate +quit
